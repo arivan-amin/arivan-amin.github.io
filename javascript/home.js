a23b8bed6c0b46c9e17853b3b8d5a46f6600b832
arivan-amin <arivanamin@gmail.com>
2020-07-31 11:54:43 +0300

    create icons row

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,10 @@
 $(() => {
-    console.log("hello");
+    setDate();
+    setInterval(setDate, 1000);
 });
+
+function setDate() {
+    let date = new Date();
+    $("#clock-text").text(date.getHours() + ":" + date.getMinutes());
+}
+        
