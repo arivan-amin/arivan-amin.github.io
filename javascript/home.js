2add6490fb44eedd25daaca77d7c10aebe5083bb
arivan-amin <arivanamin@gmail.com>
2020-08-07 11:40:19 +0300

    create search bar functionality

--- javascript/home.js
+++ javascript/home.js
@@ -1,10 +1,25 @@
 $(() => {
     setDate();
     setInterval(setDate, 1000);
+    searchGoogleSearch();
 });
 
 function setDate() {
     let date = new Date();
     $("#clock-text").text(date.getHours() + ":" + date.getMinutes());
 }
-        
+
+function searchGoogleSearch() {
+    $("#searchInput").keyup(e => {
+        if (e.keyCode === 13) {
+            searchGoogleForText($("#searchInput").val());
+        }
+    });
+    $("#searchIconDiv").click(() => {
+        searchGoogleForText($("#searchInput").val());
+    });
+}
+
+function searchGoogleForText(text) {
+    window.location = "https://www.google.com/search?q=" + text + "&ie=UTF-8";
+}
