7ac99c814531ce857c356cd20833509e8681ad79
arivan-amin <arivanamin@gmail.com>
2020-09-10 20:17:27 +0300

    create day month and year stats

--- javascript/home.js
+++ javascript/home.js
@@ -13,11 +13,32 @@ function setDayMonthYearStats() {
     setPercentValues(dayPercent, monthPercent, yearPercent);
 }
 
+// todo 8/28/20 - fix time displayed as single digit which brakes remaining time percentage
 function getAmountOfTodayPassedPercent() {
-    let hours = new Date().getHours();
-    let minutes = new Date().getMinutes();
+    let hours = getHour();
+    let minutes = getMinute();
     let currentTimeDigits = hours + "" + minutes;
     return convertNumberIntoPercentage(currentTimeDigits, 2359);
+}
+
+function getHour() {
+    let localeTimeString = getFormattedTime();
+    return localeTimeString.substring(0, localeTimeString.indexOf(":"));
+}
+
+function getMinute() {
+    let localeTimeString = getFormattedTime();
+    return localeTimeString.substring(localeTimeString.indexOf(":") + 1);
+}
+
+function getFormattedTime() {
+    let date = new Date();
+    let options = {
+        weekday: "long", year: "numeric", month: "short",
+        day: "numeric", hour: "2-digit", minute: "2-digit"
+    };
+    let localeTimeString = date.toLocaleTimeString("en-GB", options);
+    return localeTimeString.substr(localeTimeString.lastIndexOf(",") + 1).trim();
 }
 
 function getDaysOfMonthPassedPercent() {
@@ -56,8 +77,7 @@ function setPercent(idSelector, percent) {
 }
 
 function setDate() {
-    let date = new Date();
-    $("#clock-text").text(date.getHours() + ":" + date.getMinutes());
+    $("#clock-text").text(getHour() + ":" + getMinute());
 }
 
 function searchGoogleForText() {
